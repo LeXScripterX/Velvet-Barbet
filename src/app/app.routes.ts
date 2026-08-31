8ec6462aedf2d28e2731c6329b81c368fda61698
LeXScripterX <alexisvasquez998@proton.me>
2026-08-31 12:47:06 -0500

    Modificaciion a routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,36 +1,23 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
+    canActivate: [authGuard],
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage),
   },
   {
     path: 'register',
-    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+    loadComponent: () => import('./pages/register/register.page').then((m) => m.RegisterPage),
   },
   {
-    path: 'select-slot2',
-    loadComponent: () => import('./pages/select-slot2/select-slot2.page').then( m => m.SelectSlot2Page)
-  },
-  {
-    path: 'auth',
-    loadComponent: () => import('./pages/auth/auth.page').then( m => m.AuthPage)
-  },
-  {
-    path: 'appointment',
-    loadComponent: () => import('./pages/appointment/appointment.page').then( m => m.AppointmentPage)
-  },
-  {
-    path: 'auth',
-    loadComponent: () => import('./services/auth/auth.page').then( m => m.AuthPage)
-  },
-  {
-    path: 'appointment',
-    loadComponent: () => import('./services/appointment/appointment.page').then( m => m.AppointmentPage)
+    path: 'select-slot',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/select-slot/select-slot.page').then((m) => m.SelectSlotPage),
   },
 ];
